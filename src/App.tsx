f02970928fc2cdd0fee5377557d66e8a59475096
Ana Carolina <101834138+ananazario@users.noreply.github.com>
2023-02-23 13:10:18 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Botao from "./components/Button";
+import Form from "./components/Form";
+import List from "./components/Lista";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <Botao />
+      <List />
+    </div>
+  );
+}
+
+export default App;
